23f12e326af7e54adc4a7f6ea7d60387385e9b4e
Sebas17b8 <sayala98@hotmail.com>
2020-06-05 12:39:38 -0500

    agrgando express

---
+++ routes/web_autos.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  request('http://localhost:3000/autos', (err, response, body)=>{
+    res.render('web_autos', {data:JSON.parse(body)});
+  })
+
+});
+
+router.get('/:id',(req, res, next)=>{
+  request('http://localhost:3000/autos'+ req.params.id, (err,resp, body)=>{
+    res.render('web_autos', {data:JSON.parse(body)});
+  });
+});
+module.exports = router;
